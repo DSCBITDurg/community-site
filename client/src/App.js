eef642d50b2c4bb2d77b7c5b1f1eafa4463c7edf
rohitgeddam <rohitgeddam2018@gmail.com>
2020-08-08 10:11:04 +0530

    lint fix

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Router from './Router'
-import urls from './urls';
+import Router from './Router';
 
-import Header from './components/Header/Header'
+
+import Header from './components/Header/Header';
 
 // import css file
-import './assets/css/global.css'
+import './assets/css/global.css';
 
 function App() {
     return (
