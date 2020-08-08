4a91ea4b184894ce929553c2909c4da97893c9ab
rohit geddam <rohitgeddam2018@gmail.com>
2020-08-08 20:04:05 +0530

    added material icons

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -3,6 +3,11 @@ import React from 'react';
 //import components
 
 
+//import images
+import dscIcon from '../../assets/images/dscIcon.png';
+import InstagramIcon from '@material-ui/icons/Instagram';
+import WhatsAppIcon from '@material-ui/icons/WhatsApp';
+
 //import styles
 import styles from './Footer.module.css';
 
@@ -11,37 +16,37 @@ export default function Footer(){
         <div className={styles.footerContainer}>
             
             <div className={styles.footerMain}>
-                <div className={styles.dscIcons}>
-                    <p>icon 1</p>
-                    <p>icon 2</p>
-                </div>
+             
+                <img src={dscIcon} className={styles.dscIcon}/>
+                 
+             
 
                 <div className={styles.links}>
-                    <a href="#">Home</a>
-                    <a href="#">About</a>
-                    <a href="#">Blog</a>
-                    <a href="#">Contact</a>
-                    <a href="#">Events</a>
+                    <a className={styles.link} href="#">Home</a>
+                    <a className={styles.link} href="#">About</a>
+                    <a className={styles.link} href="#">Blog</a>
+                    <a className={styles.link} href="#">Contact</a>
+                    <a className={styles.link} href="#">Events</a>
                 </div>
 
                 <div className={styles.features}>
-                    <a href="#">Collab Tool</a>
-                    <a href="#">My Badges</a>
+                    <a className={styles.link} href="#">Collab Tool</a>
+                    <a className={styles.link} href="#">My Badges</a>
                 </div>
 
                 <div className={styles.eventsAndUs}>
-                    <a href="#">Our Team</a>
-                    <a href="#">Upcomming Events</a>
-                    <a href="#">Past Events</a>
-                    <a href="#">Gallary</a>
+                    <a className={styles.link} href="#">Our Team</a>
+                    <a className={styles.link} href="#">Upcomming Events</a>
+                    <a className={styles.link} href="#">Past Events</a>
+                    <a className={styles.link} href="#">Gallary</a>
                 </div>
             </div>
 
             <div className={styles.socialHandles}>
                 <div className={styles.socialIcons}>
                     <p>Social Handles</p>
-                    <p>icon 1</p>
-                    <p>icon 2</p>
+                    <p><InstagramIcon fontSize="large"/></p>
+                    <p><WhatsAppIcon fontSize="large"/></p>
                 </div>
                 <div className={styles.email}>
                     <p>Email: bitdsc@gmail.com</p>
--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import styles from './Navbar.module.css';
 
+
+//import icons
+import PersonIcon from '@material-ui/icons/Person';
+import CardMembershipIcon from '@material-ui/icons/CardMembership';
+import GroupAddIcon from '@material-ui/icons/GroupAdd';
+
 export default function Navbar(){
     return(
         <div className={styles.navbarContainer}>
@@ -13,9 +19,9 @@ export default function Navbar(){
             </div>
 
             <div className={styles.buttonsContainer}>
-                <a href="#">CollabIcon</a>
-                <a href="#">BadgeIcon</a>
-                <a href="#">UserIcon</a>
+                <a href="#"><GroupAddIcon fontSize="large"/></a>
+                <a href="#"><CardMembershipIcon fontSize="large"/></a>
+                <a href="#"><PersonIcon fontSize="large"/></a>
             </div>
         </div>
     );
